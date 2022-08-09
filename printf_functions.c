37cedb266d34d7ab880a7b80bdb3e5a965c8dffb
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-09 06:17:14 -0700

    print functions

--- printf_functions.c
+++ printf_functions.c
@@ -93,3 +93,37 @@ int print_n(va_list n)
 	}
 	return (counter);
 }
+
+/**
+ * print_binary - Print number in to binary number
+ * @n: Variable va_list
+ *
+ * Return: count of bytes printed
+ */
+int print_binary(va_list n)
+{
+	long int number;
+	int counter = 0, index = 0, i;
+	char *tmp = malloc(sizeof(char) * 32);
+
+	number = va_arg(n, int);
+	if (number < 0)
+		number *= -1;
+
+	while (number >= 0)
+	{
+		tmp[index++] = number % 2;
+		number /= 2;
+		if (number == 0)
+			break;
+	}
+
+	for (i = index - 1; i >= 0; i--)
+	{
+		_putchar(tmp[i] + 48);
+		counter++;
+	}
+	free(tmp);
+
+	return(counter);
+}
