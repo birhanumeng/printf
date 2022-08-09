de60fd1c6d2d3aeb56d6764392284042e1b4dd97
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-08 23:20:14 -0700

    main.h

--- main.h
+++ main.h
@@ -1,32 +1,28 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct types - specifier structure for printf
+ * @p: pointer to characteres specifiers
+ * @func : function pointer to print fucntions
  */
-typedef struct print
+typedef struct types
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+	char p;
+	int (*func)(va_list);
+} print_f;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int printc(va_list l);
+int print_string(va_list s);
+int (*cmp_func(const char a))(va_list);
+int print_n(va_list n);
 
-#endif
+#endif /* MAIN_H */
